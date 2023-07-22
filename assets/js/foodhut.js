9da09263c6bff6cdf35a6ed27fdc1549569e7169
Uthmanbello <uthmanbello88@gmail.com>
2023-07-22 23:22:32 +0100

    Remove public html folder and google map

--- assets/js/foodhut.js
+++ assets/js/foodhut.js
@@ -15,17 +15,3 @@ $(document).ready(function(){
         } 
     });
 });
-
-new WOW().init();
-
-function initMap() {
-    var uluru = {lat: 12.000000, lng: 8.516667};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 8,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
- }
